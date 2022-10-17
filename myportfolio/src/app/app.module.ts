4b2c0079de43f6a87f925b0ca4275b5513e9d31e
BraianF <97342723+bfois@users.noreply.github.com>
2022-10-17 11:31:31 -0300

    Agregado de google viewer
    agregado de un modelado 3d

--- myportfolio/src/app/app.module.ts
+++ myportfolio/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { ObjectComponent } from './components/object/object.component';
+
+
 
 
 @NgModule({
@@ -14,8 +17,9 @@ import { LoginComponent } from './components/login/login.component';
     HeaderComponent,
     HomeComponent,
     LoginComponent,
-  
+    ObjectComponent
   ],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     BrowserModule,
     AppRoutingModule
