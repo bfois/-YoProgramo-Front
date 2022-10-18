56d3cde4bf9537865e79a4eb78013533fe0c5bf5
BraianF <97342723+bfois@users.noreply.github.com>
2022-10-18 10:00:47 -0300

    agregado de componentes

--- myportfolio/src/app/app.module.ts
+++ myportfolio/src/app/app.module.ts
@@ -7,6 +7,14 @@ import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { ObjectComponent } from './components/object/object.component';
+import { BienvenidaComponent } from './components/bienvenida/bienvenida.component';
+import { DescripcionComponent } from './components/descripcion/descripcion.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { FormacionComponent } from './components/formacion/formacion.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
+
 
 
 
@@ -17,12 +25,18 @@ import { ObjectComponent } from './components/object/object.component';
     HeaderComponent,
     HomeComponent,
     LoginComponent,
-    ObjectComponent
+    ObjectComponent,
+    BienvenidaComponent,
+    DescripcionComponent,
+    AboutmeComponent,
+    FormacionComponent,
+    SkillsComponent,
   ],
   schemas:[CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
